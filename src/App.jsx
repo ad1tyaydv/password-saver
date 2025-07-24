8f94d60d90517cfaefcd65f9312a2e78c955b444
Aditya <adityaydv12890@gmail.com>
2025-07-24 23:00:32 +0530

    clerk added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 import Passpage from "./components/passpage";
 import Login from "./components/login";
 
@@ -6,6 +7,15 @@ import Login from "./components/login";
 function App() {
   return (
       <Router>
+        <div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+        
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/passwords" element={<Passpage />} />
